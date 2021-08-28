531368adc362264f995ca463dc62c6caaa7a45a6
yyues <yyues>
2021-08-28 20:37:32 +0800

    更新状态码code

--- Util/Result/index.js
+++ Util/Result/index.js
@@ -1,46 +1,46 @@
 class Result {
   IDNotExist() {
     return {
-      status: 500,
-      msg: "id不存在，请检查",
-    };
+      code: 500,
+      msg: 'id不存在，请检查'
+    }
   }
   NameNotExist(name) {
     return {
-      status: 500,
-      msg: `${name}不存在，请检查`,
-    };
+      code: 500,
+      msg: `${name}不存在，请检查`
+    }
   }
   success(msg, data) {
     return {
-      status: 200,
+      code: 200,
       msg: msg,
-      data: data || null,
-    };
+      data: data || null
+    }
   }
   error(msg) {
     return {
-      status: 500,
-      msg: msg,
-    };
+      code: 500,
+      msg: msg
+    }
   }
-  waring(status, msg) {
+  waring(code, msg) {
     return {
-      status: status,
-      msg: msg,
-    };
+      code: code,
+      msg: msg
+    }
   }
   noLogin() {
     return {
-      status: 401,
-      msg: "用户尚未登录，请重新登录",
-    };
+      code: 401,
+      msg: '用户尚未登录，请重新登录'
+    }
   }
   loginOutTime() {
     return {
-      status: 401,
-      msg: "登录过期，请重新登录",
-    };
+      code: 401,
+      msg: '登录过期，请重新登录'
+    }
   }
 }
-module.exports = new Result();
+module.exports = new Result()
