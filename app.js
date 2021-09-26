13160e381b6f52668d83511998b15408f00ac715
Yyue <938778575@qq.com>
2021-09-26 15:27:03 +0800

    解决无法请求的问题

--- app.js
+++ app.js
@@ -6,47 +6,36 @@
  * @Description: app
  * @FilePath: \Base-Koa\app.js
  */
-const Koa = require("koa2");
-const { Base } = require("./Config/index");
-const app = new Koa();
-const path = require("path");
+const Koa = require('koa2')
+const { Base } = require('./Config/index')
+const app = new Koa()
+const path = require('path')
 
-const { AddRouter } = require("./bin/router.js");
+const { AddRouter } = require('./bin/router.js')
 
 //bodyParser
-const bodyParser = require("koa-bodyparser");
-app.use(bodyParser());
+const bodyParser = require('koa-bodyparser')
+app.use(bodyParser())
 
 app.use(async (ctx, next) => {
   try {
-    await next();
+    await next()
   } catch (error) {
     ctx.body = {
       code: 500,
-      msg: "服务器错误",
-      data: error,
-    };
+      msg: '服务器错误',
+      data: error
+    }
   }
-});
-//听说这样才能用到request.body
-const bodyparser = require("koa-body");
-app.use(
-  bodyparser({
-    enableTypes: ["json", "form", "text"],
-    multipart: true, // ***** 就是这个 (是否支持 multipart-formdate 的表单)
-    formidable: {
-      keepExtensions: true,
-    },
-  })
-);
+})
 
 // cors 跨域
-const cors = require("koa-cors");
-app.use(cors());
+const cors = require('koa-cors')
+app.use(cors())
 
 //static
-const KoaStatic = require("koa-static");
-app.use(KoaStatic(path.join(__dirname, "/public")));
+const KoaStatic = require('koa-static')
+app.use(KoaStatic(path.join(__dirname, '/public')))
 
 // 登录拦截
 // const FILTER = require("./Config/interceptor");
@@ -54,11 +43,11 @@ app.use(KoaStatic(path.join(__dirname, "/public")));
 
 // logger
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms 请求参数-${ctx.request.body || ctx.request.params} `)
+})
 
-AddRouter(app);
-app.listen(Base.port);
+AddRouter(app)
+app.listen(Base.port)
